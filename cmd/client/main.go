fac86fa901ba752bf4b0358013cc7c8b0d6a2bb1
JorgeToAn <jorgetosta.f@gmail.com>
2026-03-16 16:57:42 -0700

    feat: create and bind queues

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
+	const rabbitConnUrl = "amqp://guest:guest@localhost:5672/"
+
 	fmt.Println("Starting Peril client...")
+	connection, err := amqp.Dial(rabbitConnUrl)
+	if err != nil {
+		log.Fatalf("failed to connect to RabbitMQ: %v", err)
+	}
+	defer connection.Close()
+	fmt.Println("Connected to RabbitMQ")
+
+	username, err := gamelogic.ClientWelcome()
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+
+	_, queue, err := pubsub.DeclareAndBind(
+		connection,
+		routing.ExchangePerilDirect,
+		fmt.Sprintf("%s.%s", routing.PauseKey, username),
+		routing.PauseKey,
+		pubsub.SimpleQueueTransient,
+	)
+	if err != nil {
+		log.Fatalf("failed to create transient queue: %v", err)
+	}
+	fmt.Printf("Created '%s' queue\n", queue.Name)
+
+	// wait for ctrl+c
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println()
+	fmt.Println("Closing Peril client...")
 }
--- internal/pubsub/pubsub.go
+++ internal/pubsub/pubsub.go
@@ -7,6 +7,13 @@ import (
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
+type SimpleQueueType int
+
+const (
+	SimpleQueueDurable = iota
+	SimpleQueueTransient
+)
+
 func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 	data, err := json.Marshal(val)
 	if err != nil {
@@ -17,4 +24,41 @@ func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 		Body:        data,
 	})
 	return nil
+}
+
+func DeclareAndBind(
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	queueType SimpleQueueType, // SimpleQueueType is an "enum" type I made to represent "durable" or "transient"
+) (*amqp.Channel, amqp.Queue, error) {
+	ch, err := conn.Channel()
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	queue, err := ch.QueueDeclare(
+		queueName,                         // name
+		queueType == SimpleQueueDurable,   // durable
+		queueType == SimpleQueueTransient, // autoDelete
+		queueType == SimpleQueueTransient, // exclusive
+		false,                             // noWait
+		nil,                               // args
+	)
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	err = ch.QueueBind(
+		queue.Name, // name
+		key,        // key
+		exchange,   // exchange
+		false,      // noWait
+		nil,        // args
+	)
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+	return ch, queue, nil
 }
